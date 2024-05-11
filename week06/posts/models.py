c5c334f1d6ca21266a78ce725c9bced57dcb4ea8
mhdiree <mhdiree@sookmyung.ac.kr>
2024-05-11 17:13:56 +0900

    6주차 과제

--- week06/posts/models.py
+++ week06/posts/models.py
@@ -8,3 +8,11 @@ class Post (models.Model): #Model 만들기
     contentn = models.TextField('내용')
     created_at = models.DateTimeField('작성일')
     view_count = models.IntegerField('조회수', default=0)
+    
+
+class Comment(models.Model):
+    author = models.CharField(max_length=100, verbose_name='작성자')
+    text = models.TextField(verbose_name='댓글 내용')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+    likes = models.IntegerField(default=0, verbose_name='좋아요 수')
+    dislikes = models.IntegerField(default=0, verbose_name='싫어요 수')
