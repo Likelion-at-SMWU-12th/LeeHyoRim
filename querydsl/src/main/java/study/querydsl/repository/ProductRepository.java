aa9f0bffe248d93fc20cf773892861859d603cec
mhdiree <mhdiree@sookmyung.ac.kr>
2024-09-10 15:10:02 +0900

    스프링 4주차 세미나

---
+++ querydsl/src/main/java/study/querydsl/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package study.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // 기본 crud는 있음, 필요한 경우 커스텀 메서드 추가
+}
