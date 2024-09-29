c0351ff56fa0a02e3ba7aece49a28495f5e60d57
mhdiree <mhdiree@sookmyung.ac.kr>
2024-09-29 21:47:26 +0900

    스프링 7주차 세미나

--- week07/server/src/main/java/com/example/server/controller/SlowServiceController.java
+++ week07/server/src/main/java/com/example/server/controller/SlowServiceController.java
@@ -1,2 +1,23 @@
-package com.example.server.controller;public class SlowServiceController {
+package com.example.server.controller;
+
+import com.example.server.dto.Tweet;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class SlowServiceController {
+
+    @GetMapping("/api/v1/slow")
+    public ResponseEntity<List<Tweet>> slowResponse() {
+        List<Tweet> tweets = Arrays.asList(
+                new Tweet("1", "Hello World", "Author1"),
+                new Tweet("2", "Spring WebFlux", "Author2")
+        );
+
+        return ResponseEntity.ok(tweets);
+    }
 }
--- week07/server/src/main/java/com/example/server/dto/Tweet.java
+++ week07/server/src/main/java/com/example/server/dto/Tweet.java
@@ -1,2 +1,35 @@
-package com.example.server.dto;public class Tweet {
+package com.example.server.dto;
+
+public class Tweet {
+
+    private String id;
+    private String content;
+    private String author;
+
+    public Tweet(){}
+
+    public Tweet(String id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    // Getters and Setters
+    public String getId() { return id; }
+    public void setId(String Id) { this.id = id; }
+
+    public String getContent() { return content; }
+    public void setContent(String email) { this.content = content; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "id='" + id + '\'' +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
